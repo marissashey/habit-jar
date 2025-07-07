2aaca1cb4c02ca918222d37a2f2e116767d44733
marissashey <57700309+marissashey@users.noreply.github.com>
2025-07-07 14:31:15 -0400

    display timestamp

--- src/Habit.jsx
+++ src/Habit.jsx
@@ -2,17 +2,18 @@ import {useState, useEffect} from "react";
 
 /**
  * TODO:
- * - change Date instances to be Timestamps so creating multiple in a day is supported
- * - in handleButtonClick, clear the content in the inputbox, and ensure that clearing it doesn't change the state variables and subsequently change the previous marble content. aka find a way to store down the previous marble in the marble array! and then reset the state variables! and also reset `isShown` to hide the current marble (can change it to be based on whether textInput has content!)
- * - change marble text to only show on hover!
- * - add jar!
- * - add animation of marble into jar!
+ * [X] change Date instances to be Timestamps so creating multiple in a day is supported
+ * [ ] in handleButtonClick, clear the content in the inputbox, and ensure that clearing it doesn't change the state variables and subsequently change the previous marble content. aka find a way to store down the previous marble in the marble array! and then reset the state variables! and also reset `isShown` to hide the current marble (can change it to be based on whether textInput has content!)
+ * [ ] change marble text to only show on hover! and resize to size of marble!
+ * [ ] add jar!
+ * [ ] add animation of marble into jar! idea: make jar div a grid, if spot isn't taken, drop marble from directly above that spot
+ * [ ]
  */
 
 function Habit() {
   const [marbles, setMarbles] = useState([]);
   const [textInput, setTextInput] = useState("");
-  const [date, setDate] = useState("");
+  const [timestamp, setTimestamp] = useState("");
   const [isShown, setIsShown] = useState(false);
   const [marble, setMarble] = useState("");
   function handleButtonClick(e) {
@@ -21,10 +22,39 @@ function Habit() {
     // const date = new Date();
     // const todayMarble = Marble(date, textInput);
     // setMarble(todayMarble);
-    setIsShown(true);
-    const today = new Date();
 
-    setDate(today.toDateString().toLowerCase());
+    const userLocale = navigator.language || navigator.userLanguage || "en-US";
+    console.log(userLocale);
+
+    setIsShown(true);
+    /*
+    new Intl.DateTimeFormat("en-IN",
+      (options = {
+        dateStyle: "long", timeStyle: "long"
+      })
+    ).format(new Date());
+    // "7 July 2025 at 9:56:19 pm IST"
+    const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+    console.log(timezone); // e.g., "America/New_York"
+    const userLocale = navigator.language || navigator.userLanguage || 'en-US';
+    console.log(userLocale);
+    */
+    const formatter = new Intl.DateTimeFormat(userLocale, {
+      dateStyle: "short",
+      timeStyle: "short",
+      // weekday: "narrow",
+      // hour: "numeric",
+      // minute: "numeric",
+      // dayPeriod: "short",
+      // month: "short",
+      // day: "numeric",
+      // year: "numeric",
+    });
+
+    const today = formatter.format(new Date());
+    console.log("today: ", today);
+
+    setTimestamp(today.toLowerCase());
   }
 
   function handleInputChange(e) {
@@ -34,9 +64,13 @@ function Habit() {
     console.log("textInput: ", textInput);
   }, [textInput]);
 
+  useEffect(() => {
+    console.log("timestamp: ", timestamp);
+  }, [timestamp]);
+
   return (
     <>
-      <div>
+      <div className='font-poppins color-X'>
         <h1>title: habit jars</h1>
         <h2> description: my minimalist productivity tool</h2>
         <TodayBox
@@ -50,7 +84,7 @@ function Habit() {
         {/* show element on click */}
         {isShown && (
           <div>
-            <div className='relative w-50 h-50 bg-no-repeat bg-contain bg-[url(marble.svg)]'>
+            <div className='relative w-20 h-20 bg-no-repeat bg-contain bg-[url(wonky_marble.svg)]'>
               {/* <img
                 alt='marble pixel art'
                 src='marble.svg'
@@ -58,9 +92,11 @@ function Habit() {
                 title='hi'
               /> */}
               <div className='absolute inset-0 flex justify-center items-center text-center'>
-                {date} <br /> {textInput}
+                {timestamp} <br /> {textInput}
               </div>
             </div>
+
+            <div className='relative w-50 h-50 bg-no-repeat bg-contain bg-[url(wonky_jar.svg)]'></div>
           </div>
         )}
         <ul>
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import {StrictMode} from "react";
 import {createRoot} from "react-dom/client";
 import "./style.css";
-import App from "./App.jsx";
+import Habit from "./Habit.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <Habit />
   </StrictMode>
 );
